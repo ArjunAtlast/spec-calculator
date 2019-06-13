fecb9e8ed5dcffd4a908a3a26da468a6df856c94
Arjun P <arjunatlast@gmail.com>
2019-06-13 16:43:46 +0530

    Delete env.py

--- env.py
+++
@@ -1,5 +0,0 @@
-#Twitter Credentials
-token = "3879702192-iePpyi1RBOu4U8GDYWBneJsusMaZtXEKOgtWSql"
-secret = "Q51lZIhlpYW0rVwBo9JnqOmnPblhmnuBVKbJBKbHWhYqu"
-consumer_key = "Ht5dtcU8D4h6RiPcAUmE792dc"
-consumer_secret = "pd1ZfObtM5MfobP6Jwq2oMDC2BYFNWP9Wvodsh7TDp76bBHLOQ"
